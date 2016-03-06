eca574191e5347476049071f39abf03eefe0f702
holzig <holzigg@gmail.com>
2016-03-06 08:58:58 +0100

    spike test cleanup

--- src/test/java/cma/cards/PdfSpike.java
+++ src/test/java/cma/cards/PdfSpike.java
@@ -1,27 +1,14 @@
 package cma.cards;
 
-import java.awt.geom.AffineTransform;
-
-import org.apache.fontbox.util.BoundingBox;
 import org.junit.Test;
 
 public class PdfSpike {
 
-	private static final float POINTS_PER_INCH = 72;
-	private static final float MM_PER_INCH = 1 / (10 * 2.54f) * POINTS_PER_INCH;
+	private PdfService service = new PdfService();
 
-	private PdfService willi = new PdfService(); 
-	
 	@Test
 	public void testName() throws Exception {
-		
-		String text = "Hanna";
-		// Create a new empty document
-		willi.createPdf(text);
-	}
-	
-	private double ptToMm(float width) {
-		return width * 25.4 / 72;
+		service.createPdf("Test");
 	}
 
 }
